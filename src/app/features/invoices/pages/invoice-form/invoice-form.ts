2136b3ac4cd00e8d886f54defd07421a81507afb
Cristiam Sanchez <cristiam.sanchez204@gmail.com>
2026-08-26 21:58:54 -0600

    chore: create initial Angular application

---
+++ src/app/features/invoices/pages/invoice-form/invoice-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-invoice-form',
+  styleUrl: './invoice-form.scss',
+  templateUrl: './invoice-form.html',
+})
+export class InvoiceForm {}
---
+++ src/app/features/quotations/pages/quotation-form/quotation-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-quotation-form',
+  styleUrl: './quotation-form.scss',
+  templateUrl: './quotation-form.html',
+})
+export class QuotationForm {}
